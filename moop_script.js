ecf34ea2e15e1a1019e827d7cf1610b1828f1362
Tulio Ferreira <tuliof210@gmail.com>
2020-02-20 15:04:25 -0300

    Antes da merda acontecer

--- moop_script.js
+++ moop_script.js
@@ -7,18 +7,22 @@ function myFunction() {
     for (let i = 1; i <= n_value; i++) {
         document.getElementById("xminHolder").innerHTML += `
         <div class="col-xs-3">
-            <input class="form-control col-xs-3" type="number" id="x_min_${i}" name="x_min_1" value="-10">
+            <input class="form-control col-xs-3" type="number" id="x_min_${i}" name="x_min_1" value="-10" min="-9999" max="9999">
         </div>
         `;
         document.getElementById("xmaxHolder").innerHTML += `
         <div class="col-xs-3">
-            <input class="form-control" type="number" id="x_max_${i}" name="x_max_1" value="10">
+            <input class="form-control" type="number" id="x_max_${i}" name="x_max_1" value="10" min="-9999" max="9999">
         </div>
         `;
     }
 }
 
 function sendRequest() {
+
+    //reseta o espaço da resposta
+    place_reponse.classList.add("response-place");
+
     let resultElement = document.getElementById('response');
     let errorElement = document.getElementById('error');
     resultElement.innerHTML = null;
@@ -58,6 +62,7 @@ function sendRequest() {
             eps: eps_value
         }));
         console.log('oioioi');
+
         xhr.onload = function() {
             console.log("Response")
             console.log(this.responseText);
@@ -66,8 +71,7 @@ function sendRequest() {
             // resultElement.innerHTML =  "Stop criterion: " + JSON.stringify(data["message"]) + "<br>" + "x* = " + JSON.stringify(data["x"])  + "<br>" + "f(x*) = " + JSON.stringify(data["fx"]) ;
             //resultElement.innerHTML = JSON.stringify(data["x"]);
 
-            //libera espaço para a resposta seja exibida
-            place_reponse.classList.toggle("response-place");
+
 
             var trace3 = {
                 x: data["fx"][0],
@@ -85,16 +89,60 @@ function sendRequest() {
             };
             var data_plot = [trace3];
             Plotly.newPlot('response_plot', data_plot, layout);
+
+            //libera espaço para a resposta seja exibida
+            place_reponse.classList.remove("response-place");
         }
         xhr.onerror = function(e) {
-            //libera espaço para a resposta seja exibida
-            place_reponse.classList.toggle("response-place");
+            let errobody = document.querySelector('main')
+            errobody.innerHTML += `
+                <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            `
+
             console.log("Ooops, não funcionou: ", e)
             errorElement.innerHTML = "Não funcionou";
         }
     } catch (e) {
-        //libera espaço para a resposta seja exibida
-        place_reponse.classList.toggle("response-place");
+        let errobody = document.querySelector('main')
+        errobody.innerHTML += `
+            <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+            </div>
+        `
         console.log('Deu merda', e);
     }
 }
