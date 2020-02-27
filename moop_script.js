d502cf4836d7035425582045dfd4fc390420825e
Tulio Ferreira <tuliof210@gmail.com>
2020-02-27 12:56:24 -0300

    Resolvendo problema .sub()

--- moop_script.js
+++ moop_script.js
@@ -169,10 +169,10 @@ function drawChart() {
     var options = {
         title: 'Pareto front',
         hAxis: {
-            title: 'f11(x)'
+            title: 'f₁(x)'
         },
         vAxis: {
-            title: 'f1(x)'
+            title: 'f₂(x)'
         },
         legend: 'none',
         width: $(mainWidth).width(), //pega a largura do main
